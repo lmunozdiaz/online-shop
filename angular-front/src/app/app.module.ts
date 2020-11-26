210d48da7de0aaabd9596784dadba47dcc1dfc69
louis diaz <lemunoz8@gmail.com>
2020-11-26 16:13:04 -0600

    Added Angular Material support

--- angular-front/src/app/app.module.ts
+++ angular-front/src/app/app.module.ts
@@ -7,17 +7,23 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from "@angular/common/http";
 import { ProductService } from "./services/product.service";
 import { ProductListComponent } from './components/product-list/product-list.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import {MaterialModule} from "./material/material.module";
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
+    LayoutModule,
+    MaterialModule,
   ],
   providers: [ProductService],
   bootstrap: [AppComponent]
--- angular-front/src/app/material/material.module.ts
+++ angular-front/src/app/material/material.module.ts
@@ -1,12 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+// material dependencies
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    MatIconModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatSidenavModule,
+  ],
+  exports: [
+    MatIconModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatSidenavModule,
   ]
 })
 export class MaterialModule { }
