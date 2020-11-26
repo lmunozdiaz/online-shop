6856a757e85817691a11ea89d0f7d48fda5eaa1a
louis diaz <lemunoz8@gmail.com>
2020-11-26 16:00:36 -0600

    Enabled CrossOrigin for current controllers

--- src/main/java/cs/roosevelt/onlineshop/controller/ProductCategoryController.java
+++ src/main/java/cs/roosevelt/onlineshop/controller/ProductCategoryController.java
@@ -3,11 +3,13 @@ package cs.roosevelt.onlineshop.controller;
 import cs.roosevelt.onlineshop.entity.ProductCategory;
 import cs.roosevelt.onlineshop.service.ProductCategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 public class ProductCategoryController {
     @Autowired
--- src/main/java/cs/roosevelt/onlineshop/controller/ProductController.java
+++ src/main/java/cs/roosevelt/onlineshop/controller/ProductController.java
@@ -3,11 +3,13 @@ package cs.roosevelt.onlineshop.controller;
 import cs.roosevelt.onlineshop.entity.Product;
 import cs.roosevelt.onlineshop.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 public class ProductController {
     @Autowired
